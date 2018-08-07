79d429924bd2089acac5da0a58af452f672400a2
Scott Weinstein <sweinstein27@gmail.com>
2018-08-07 14:47:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -8,39 +7,40 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/posts/new' do
+  get '/posts/new' do #loads new form
     erb :new
   end
 
-  post '/posts' do
+  post '/posts' do  #creates a post
     @post = Post.create(params)
     redirect to '/posts'
   end
 
-  get '/posts' do
+  get '/posts' do #loads index page
     @posts = Post.all
     erb :index
   end
 
-  get '/post/:id' do
+  get '/posts/:id' do  #loads show page
     @post = Post.find_by_id(params[:id])
     erb :show
   end
 
-  get '/posts/:id/edit' do
+  get '/posts/:id/edit' do #loads edit form
     @post = Post.find_by_id(params[:id])
     erb :edit
   end
 
-  patch '/posts/:id' do
+  patch '/posts/:id' do  #updates a post
     @post = Post.find_by_id(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
-    @post.saves
+    @post.save
     erb :show
   end
 
-  delete '/posts/:id/delete' do
+
+  delete '/posts/:id/delete' do #delete action
     @post = Post.find_by_id(params[:id])
     @post.delete
     erb :deleted
